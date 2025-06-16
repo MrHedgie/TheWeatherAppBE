e7ca0973f56f367d8eaf9ace0ef2a521045d41d3
Adam <madaoksap@gmail.com>
2025-06-16 17:15:35 +0200

    feat: initial project files

---
+++ src/main/java/com/example/theweatherapp/TheWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.theweatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheWeatherAppApplication.class, args);
+	}
+
+}
