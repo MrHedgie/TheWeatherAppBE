89768620c2b74daec3ef75a14ce44521f348a2fb
Adam <madaoksap@gmail.com>
2025-06-16 19:31:07 +0200

    refactor: slight refactor to WeatherApiResponse

--- src/main/java/com/example/theweatherapp/model/WeatherApiResponse.java
+++ src/main/java/com/example/theweatherapp/model/WeatherApiResponse.java
@@ -1,7 +1,8 @@
 package com.example.theweatherapp.model;
 
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record WeatherApiResponse(@JsonProperty("daily") DailyData dailyData) {
+public record WeatherApiResponse(
+        @JsonProperty("daily") DailyData dailyData
+) {
 }
